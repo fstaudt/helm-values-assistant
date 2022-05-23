806316d272e8c785dc7d1d765264f1cd72740943
GitHub Action <action@github.com>
2022-05-23 11:43:52 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/fstaudt/helmvaluesassistant/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.fstaudt.helmvaluesassistant.services
+
+import com.github.fstaudt.helmvaluesassistant.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
